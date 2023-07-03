57b6010f5665c1803f3840c061d50f92ee6459fd
Fabian Rodriguez <daylhertrodriguez@gmail.com>
2023-07-03 00:15:37 -0500

    Streaming video desde el backend al frontend

--- app/router/detect.py
+++ app/router/detect.py
@@ -1,25 +1,65 @@
 import base64
-from fastapi import APIRouter, Request
-
+import subprocess
+import aiohttp
+from fastapi import WebSocket, WebSocketDisconnect
+import time
+import cv2
+from fastapi import APIRouter
+from app.utils.sockets import sio_server
+from app.utils.modelling import run_object_detection
 from app.utils.notification import send_whatsapp_message
 
 router = APIRouter()
 
-@router.post("/detect-weapon")
-async def detect(request: Request):
-    print(request)
-    # # Obtener la imagen del request
-    # data = await request.json()
-    # img_data = data['image'].split(',', 1)[1]
-    # img_data = base64.b64decode(img_data)
-    # image = Image.open(io.BytesIO(img_data))
+@router.websocket("/ws")
+async def get_stream(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        cap = cv2.VideoCapture(1)
 
-    # # Pasar la imagen al modelo YOLO
-    # objetos_detectados = detectar_objeto(image)
+        while True:
+            success, frame = cap.read()
+            if not success:
+                break
+            else:
+                #detection
+                # command = "python yolov5/detect.py --source 0 --conf 0.25 --weights yolov5/runs/train/exp4/weights/best.pt"
+                # process = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                # out, _ = process.communicate(input=frame.tobytes())
+                # process.wait()
 
-    # # Si se detecta un objeto, enviar una notificación (esto es solo un ejemplo)
-    # if objetos_detectados:
-    #     send_whatsapp_message(objetos_detectados)
+                # # Obtener el output
+                # detection_output = out.decode()
 
-    # # Devolver una respuesta
-    # return {'objetos_detectados': objetos_detectados}
+                # # Process the detection results
+                # lines = detection_output.strip().split("\n")
+                # results = []
+                # for line in lines[1:]:
+                #     if line.startswith("image"):
+                #         break
+                #     items = line.split()
+                #     label = items[0]
+                #     confidence = float(items[1])
+                #     xmin, ymin, xmax, ymax = map(int, items[2:6])
+                #     results.append({
+                #         "label": label,
+                #         "confidence": confidence,
+                #         "bbox": (xmin, ymin, xmax, ymax)
+                #     })
+
+                # #Visualize the detection results on the frame
+                # for result in results:
+                #     label = result["label"]
+                #     confidence = result["confidence"]
+                #     xmin, ymin, xmax, ymax = result["bbox"]
+
+                #     cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
+                #     cv2.putText(frame, f"{label}: {confidence:.2f}", (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+                #Transmision
+                _, buffer = cv2.imencode('.jpg', frame)
+                frame_base64 = base64.b64encode(buffer).decode('utf-8')
+                await websocket.send_bytes(frame_base64)
+
+    except WebSocketDisconnect:
+        print("Client disconnected")
--- main.py
+++ main.py
@@ -15,10 +15,9 @@ app = FastAPI(
 )
 
 # CORS
-origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
